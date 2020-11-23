5edb5312b02b71606cfc2cba1ca6b15ef72b1e76
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-23 11:31:04 +0300

    Setup route for blog and menu page with partials setup

--- routes/main.js
+++ routes/main.js
@@ -11,4 +11,21 @@ router.get('/', (req, res, next) => {
     res.render('home', data)
 })
 
+router.get('/blog', (req, res, next) => {
+    const data = {
+        title: 'Blog Page | Coffee',
+        description: 'Coffe Home,  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+    }
+
+    res.render('blog', data)
+})
+router.get('/menu', (req, res, next) => {
+    const data = {
+        title: 'Menu | Coffee',
+        description: 'Coffe Home,  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+    }
+
+    res.render('menu', data)
+})
+
 module.exports = router 
