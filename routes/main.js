bb5ad4389ae6774e2be1bb18eeac4e9f7c9f8e21
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-23 11:24:05 +0300

    Head and scripts partial

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ router.get('/', (req, res, next) => {
     // res.send('this is main route')
     const data = {
         title: 'Coffee Home',
-        description: 'Coffe Home, Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+        description: 'Coffe Home,  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
     }
 
     res.render('home', data)
