f17ed1ad0d8e9737fd634c82173ad0ec6d6d9cef
Sivar Sarkawt <fragmatica.dc@gmail.com>
2020-11-23 11:18:15 +0300

    Navigation partial

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    res.send('this is main route')
+    // res.send('this is main route')
+    const data = {
+        title: 'Coffee Home',
+        description: 'Coffe Home, Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.'
+    }
+
+    res.render('home', data)
 })
 
 module.exports = router 
